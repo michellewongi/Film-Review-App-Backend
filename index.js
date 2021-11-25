5c6355cae651a53afad75ed4db7e690470d54c12
mw3981 <mw3981@nyu.edu>
2021-11-25 15:22:56 -0500

    starter files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
